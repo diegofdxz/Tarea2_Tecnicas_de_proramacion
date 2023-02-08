55be942b0b43823e5c7207742a2a6ae39f91ad8b
diegofdxz <113787026+diegofdxz@users.noreply.github.com>
2023-02-08 00:25:07 +0000

    commit1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,1 +1,15 @@
+/*
+ * Ejercicio 1
+ */
+class Ejercicio1 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 3;
+    System.out.println("A = " + a);
+    b = a + 5;
 
+    System.out.println("A = " + a + " y B = " + b);
+    a = 7;
+    System.out.println("A = " + a + " y B = " + b);
+    }
+}

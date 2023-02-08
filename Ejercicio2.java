40c2954d34bd7c51b29b51f2bfe7a66fe22ee964
diegofdxz <113787026+diegofdxz@users.noreply.github.com>
2023-02-08 00:34:16 +0000

    commit1

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,1 +1,16 @@
-
+/*
+ * Ejercicio 2
+ * 
+ */
+class Ejercicio2 {
+    public static void main(String[] args) {
+        int valor1, valor2;
+        String cadena1;
+        final double CST = 49.78;
+        valor1 = 92 % 8;
+        valor2 = 2 * valor1;
+        cadena1 = "Test";
+        System.out.println(cadena1 + ", valor 2 = " + valor2);
+        System.out.println(valor1 + " # " + CST);
+    }
+}
